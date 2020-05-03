48cdc6f7ee0830744e864893b4375be62db72c4f
Riv <rivian@protonmail.com>
2020-05-03 13:19:03 +0200

    Separate with newline

--- vscode/updater-windows.ps1
+++ vscode/updater-windows.ps1
@@ -1,6 +1,7 @@
 Write-Host "Copying settings.json"
 Copy-Item "$env:APPDATA\Code\User\settings.json" "$(Get-Location)\settings.json"
 Write-Host "Finished copying settings.json"
+
 Write-Host "Copying keybindings.json"
 Copy-Item "$env:APPDATA\Code\User\keybindings.json" "$(Get-Location)\keybindings.json"
 Write-Host "Finished copying keybindings.json"
